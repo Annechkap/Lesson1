90985b69443464102ac0da3b38e8f1c179f2a143
Anna <annechkap@list.ru>
2022-10-03 15:59:26 +0300

    Задача1

---
+++ Task0/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само сеья), например: 4->16, 3->9, 7->49
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string inputString = Console.ReadLine();
+    int value = Convert.ToInt32(inputString);
+    return value;
+}
+
+int value = Prompt("Введите число >   ");
+int square = value * value;
+Console.WriteLine($"Квадрат числа {value} равен {square}");
